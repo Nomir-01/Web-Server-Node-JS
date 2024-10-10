6886ec46bc4599865eda72d2b2d0600feaf5280d
Nomir-01 <nomirnasir@gmail.com>
2024-10-11 00:38:27 +0500

    "Cart"

--- nodejs/server.js
+++ nodejs/server.js
@@ -5,6 +5,7 @@ app.use(express.urlencoded({ extended: true }));
 
 //GET, POST, PUT, PATCH, DELETE
 
+//Users
 const userPath = "/users";
 const users = [
   {
@@ -22,6 +23,7 @@ const users = [
       apartmentName: "Al-Haram Corner",
     },
     phoneNumber: "0334-9256885",
+    cartid: [1, 2],
   },
   {
     id: 2,
@@ -38,6 +40,7 @@ const users = [
       apartmentName: "Reno Centre",
     },
     phoneNumber: "0300-1234567",
+    cartid: [3],
   },
   {
     id: 3,
@@ -54,6 +57,7 @@ const users = [
       apartmentName: "Kali Centre",
     },
     phoneNumber: "0301-1234567",
+    cartid: [4],
   },
   {
     id: 4,
@@ -70,22 +74,39 @@ const users = [
       apartmentName: "Qureshi Residency",
     },
     phoneNumber: "0302-1234567",
+    cartid: [0],
   },
 ];
 
 //Add User(s)
 app.post(userPath, (req, res) => {
   const body = req.body;
+  const keys = Object.keys(body);
   if (body?.name) {
-    const user = {
-      id: users.length + 1,
-      name: body.name,
-      email: body.email,
-      username: body.username,
-      name: body.name,
-      address: body.address,
-      phoneNumber: body.phoneNumber,
-    };
+    let user = {};
+    if (keys.includes("cartid")) {
+      user = {
+        id: users.at(-1).id + 1,
+        name: body.name,
+        email: body.email,
+        username: body.username,
+        name: body.name,
+        address: body.address,
+        phoneNumber: body.phoneNumber,
+        cartid: body.cartid,
+      };
+    } else {
+      user = {
+        id: users.at(-1).id + 1,
+        name: body.name,
+        email: body.email,
+        username: body.username,
+        name: body.name,
+        address: body.address,
+        phoneNumber: body.phoneNumber,
+        cartid: 0,
+      };
+    }
     users.push(user);
     res.status(201).json(user);
   }
@@ -155,6 +176,13 @@ app.delete(`${userPath}/:id`, (req, res, next) => {
     if (user === -1) {
       res.status(404).send("User not found");
     } else {
+      const cartsToDel = users[user].cartid;
+      for (let a = 0; a < cartsToDel.length; a++) {
+        const cart = carts.findIndex((c) => {
+          return c.id === Number(cartsToDel[a]);
+        });
+        carts.splice(cart, 1);
+      }
       users.splice(user, 1);
       res.status(200).send("User is deleted");
     }
@@ -163,6 +191,7 @@ app.delete(`${userPath}/:id`, (req, res, next) => {
   res.status(400).send("Please send a correct User ID");
 });
 
+//Products
 const productPath = "/products";
 const products = [
   {
@@ -325,7 +354,7 @@ app.post(productPath, (req, res) => {
   const body = req.body;
   if (body?.title) {
     const product = {
-      id: products.length + 1,
+      id: products.at(-1).id + 1,
       title: body.title,
       price: body.price,
       category: body.category,
@@ -354,8 +383,27 @@ app.patch(`${productPath}/:id`, (req, res, next) => {
     } else {
       for (const value of keys) {
         if (value in products[product]) {
-          products[product][value] = body[value];
-          console.log(products[product][value]);
+          if (value == "quantity") {
+            if (products[product][value] >= 1 && body[value] == 0) {
+              products[product][value] = body[value];
+              products[product].inStock = false;
+              console.log(products[product][value]);
+              console.log(products[product].inStock);
+              console.log(body[value]);
+            } else if (products[product][value] == 0 && body[value] >= 1) {
+              products[product][value] = body[value];
+              products[product].inStock = true;
+              console.log(products[product][value]);
+              console.log(products[product].inStock);
+              console.log(body[value]);
+            } else if (body[value] < 0) {
+              res.status(406).send("Wrong Input For Quantity");
+            } else {
+              products[product][value] = body[value];
+            }
+          } else if (value != "quantity") {
+            products[product][value] = body[value];
+          }
         } else {
           res
             .status(406)
@@ -386,6 +434,174 @@ app.delete(`${productPath}/:id`, (req, res, next) => {
   }
 
   res.status(400).send("Please send a correct Product ID");
+});
+
+//Cart
+const cartPath = "/carts";
+const carts = [
+  {
+    id: 1,
+    userId: 1,
+    date: "2019-12-15",
+    products: [
+      { productId: 1, quantity: 10 },
+      { productId: 5, quantity: 1 },
+    ],
+  },
+  {
+    id: 2,
+    userId: 1,
+    date: "2022-01-21",
+    products: [
+      { productId: 9, quantity: 4 },
+      { productId: 5, quantity: 2 },
+    ],
+  },
+  {
+    id: 3,
+    userId: 2,
+    date: "2024-06-17",
+    products: [
+      { productId: 4, quantity: 4 },
+      { productId: 8, quantity: 1 },
+    ],
+  },
+  {
+    id: 4,
+    userId: 3,
+    date: "2020-10-07",
+    products: [
+      { productId: 3, quantity: 3 },
+      { productId: 7, quantity: 5 },
+    ],
+  },
+];
+
+//All Cart(s)
+app.get(cartPath, (req, res, next) => {
+  res.status(200).json(carts);
+});
+
+//Single Cart
+app.get(`${cartPath}/:id`, (req, res, next) => {
+  const query = req.params;
+  if (query?.id) {
+    const cart = carts.find((c) => {
+      return c.id === Number(query.id);
+    });
+
+    if (cart) {
+      res.status(200).json(cart);
+    } else {
+      res.status(404).send("Cart not found");
+    }
+  }
+
+  res.status(400).send("Please send a correct Cart ID");
+});
+
+//Single User Cart(s)
+app.get(`${cartPath}/:id1/:id2`, (req, res, next) => {
+  const query = req.params;
+  if (query?.id2) {
+    const cartToShow = [];
+    console.log(query.id2);
+    for (const a of carts) {
+      if (a.userId == query.id2) {
+        console.log(a);
+        cartToShow.push(a);
+      }
+    }
+    if (cartToShow.length >= 1) {
+      res.status(200).json(cartToShow);
+    } else {
+      res.status(404).send("No Cart Created");
+    }
+  }
+
+  res.status(400).send("Please send a correct Cart ID");
+});
+
+//Add Cart(s)
+app.post(cartPath, (req, res) => {
+  const body = req.body;
+  if (body?.products) {
+    const cart = {
+      id: carts.at(-1).id + 1,
+      userId: body.userId,
+      date: body.date,
+      products: body.products,
+    };
+    carts.push(cart);
+    res.status(201).json(cart);
+  }
+  res.status(400).send("Please add required info");
+});
+
+//Update Cart(s)
+app.patch(`${cartPath}/:id`, (req, res, next) => {
+  const query = req.params;
+  const body = req.body;
+  const keys = Object.keys(body);
+  if (query?.id) {
+    const cart = carts.findIndex((c) => {
+      return c.id === Number(query.id);
+    });
+
+    if (cart === -1) {
+      res.status(404).send("Cart not found");
+    } else {
+      for (const value of keys) {
+        if (value in carts[cart]) {
+          if (value == "products") {
+            const toCheck = body[value].length;
+            console.log(toCheck);
+            for (let a = 0; a < toCheck; a++) {
+              console.log(body[value][a].quantity);
+              if (body[value][a].quantity == 0 && body[value].length > 1) {
+                body[value].splice(a, 1);
+              } else if (
+                body[value][a].quantity == 0 &&
+                body[value].length <= 1
+              ) {
+                carts.splice(cart, 1);
+                res.status(200).send("Cart Was Deleted Because Of No Products");
+              }
+            }
+            console.log(body[value]);
+          }
+          carts[cart][value] = body[value];
+        } else {
+          res
+            .status(406)
+            .send("Update can't be completely done due to an unknown field");
+        }
+      }
+
+      res.status(200).json(carts[cart]);
+    }
+  }
+
+  res.status(400).send("Please send a correct User ID");
+});
+
+//Delete Cart(s)
+app.delete(`${cartPath}/:id`, (req, res, next) => {
+  const query = req.params;
+  if (query?.id) {
+    const cart = carts.findIndex((c) => {
+      return c.id === Number(query.id);
+    });
+
+    if (cart === -1) {
+      res.status(404).send("Cart not found");
+    } else {
+      carts.splice(cart, 1);
+      res.status(200).send("Cart is deleted");
+    }
+  }
+
+  res.status(400).send("Please send a correct Cart ID");
 });
 
 app.listen(3001, () => {
